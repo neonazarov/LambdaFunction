862c310fc38cfd520f279ae971f671a67ed5ce55
neonazarov <nazarovtec@gmail.com>
2025-02-28 19:13:57 +0500

    8

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -16,8 +16,7 @@ import java.util.Map;
 @LambdaHandler(
     lambdaName = "sns_handler",
 	roleName = "sns_handler-role",
-	isPublishVersion = true,
-	aliasName = "${lambdas_alias_name}",
+	isPublishVersion = false,
 	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @SnsEventSource(targetTopic = "lambda_topic", regionScope = RegionScope.DEFAULT)
